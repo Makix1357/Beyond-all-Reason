5a15f54f0c47602e70a6190f6490a3edbb5086b1
Pluto <80173508+Makix1357@users.noreply.github.com>
2025-05-16 12:20:52 +0200

    Update Buildoptions for Nanos.lua
    deepcopy of buildoptions

--- Buildoptions for Nanos.lua
+++ Buildoptions for Nanos.lua
@@ -1,12 +1,24 @@
-Buildoptions for Nanos
-
 --Building Nanos
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else 
+        copy = orig
+    end
+    return copy
+end
 local UnitDefs = UnitDefs or {}
 local Factions = {"arm", "cor", "leg"}
 for _, faction in ipairs(Factions) do
     local units = faction.."nanotct2"
      if UnitDefs[units] then
-        UnitDefs[units].buildoptions = UnitDefs[faction.."ack"].buildoptions
+        UnitDefs[units].buildoptions = deepcopy(UnitDefs[faction.."ack"].buildoptions)
         table.insert(UnitDefs[units].buildoptions, faction.."aap")
         table.insert(UnitDefs[units].buildoptions, faction.."avp")
         table.insert(UnitDefs[units].buildoptions, faction.."wint2")
@@ -22,13 +34,13 @@ for _, faction in ipairs(Factions) do
     local units = {faction.."nanotc", faction.."respawn"}
     for _, unit in ipairs(units) do
         if UnitDefs[unit] then
-            local baseBuildOptions = UnitDefs[faction.."ck"].buildoptions
+            local baseBuildOptions = deepcopy(UnitDefs[faction.."ck"].buildoptions)
             UnitDefs[unit].buildoptions = {}
             for i, option in ipairs(baseBuildOptions) do
                 UnitDefs[unit].buildoptions[i] = option
             end
-            table.insert(UnitDefs[unit].buildoptions, faction.."aap")
-            table.insert(UnitDefs[unit].buildoptions, faction.."avp")
+            table.insert(UnitDefs[unit].buildoptions, faction.."ap")
+            table.insert(UnitDefs[unit].buildoptions, faction.."vp")
         end
     end
 end
