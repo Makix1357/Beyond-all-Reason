4dde09fa0ab085d21704961b263329a53d0665ae
Pluto <80173508+Makix1357@users.noreply.github.com>
2025-05-16 13:14:23 +0200

    Update Buildoptions for Nanos.lua

--- Buildoptions for Nanos.lua
+++ Buildoptions for Nanos.lua
@@ -1,46 +1,62 @@
 --Building Nanos
 function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
-        end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
-    else 
-        copy = orig
-    end
-    return copy
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		copy = {}
+		for orig_key, orig_value in next, orig, nil do
+			copy[deepcopy(orig_key)] = deepcopy(orig_value)
+		end
+		setmetatable(copy, deepcopy(getmetatable(orig)))
+	else
+		copy = orig
+	end
+	return copy
 end
 local UnitDefs = UnitDefs or {}
-local Factions = {"arm", "cor", "leg"}
+local Factions = { 'arm', 'cor', 'leg' }
+
+--T1 Buildoptions
 for _, faction in ipairs(Factions) do
-    local units = faction.."nanotct2"
-     if UnitDefs[units] then
-        UnitDefs[units].buildoptions = deepcopy(UnitDefs[faction.."ack"].buildoptions)
-        table.insert(UnitDefs[units].buildoptions, faction.."aap")
-        table.insert(UnitDefs[units].buildoptions, faction.."avp")
-        table.insert(UnitDefs[units].buildoptions, faction.."wint2")
-        table.insert(UnitDefs[units].buildoptions, faction.."afust3")
-        if faction == "arm" or faction == "cor" then
-            table.insert(UnitDefs[units].buildoptions, faction.."mmkrt3")
-        else 
-            table.insert(UnitDefs[units].buildoptions, faction.."adveconvt3")
-        end
-    end
+	local units = { faction .. 'nanotc', faction .. 'respawn', 'legnanotcbase' }
+	for _, unit in ipairs(units) do
+		if UnitDefs[unit] then
+			local baseBuildOptions = deepcopy(UnitDefs[faction .. 'ck'].buildoptions)
+			UnitDefs[unit].buildoptions = {}
+			for i, option in ipairs(baseBuildOptions) do
+				UnitDefs[unit].buildoptions[i] = option
+			end
+			table.insert(UnitDefs[unit].buildoptions, faction .. 'ap')
+			table.insert(UnitDefs[unit].buildoptions, faction .. 'vp')
+		end
+	end
 end
+
+--T2 Buildoptions
 for _, faction in ipairs(Factions) do
-    local units = {faction.."nanotc", faction.."respawn"}
-    for _, unit in ipairs(units) do
-        if UnitDefs[unit] then
-            local baseBuildOptions = deepcopy(UnitDefs[faction.."ck"].buildoptions)
-            UnitDefs[unit].buildoptions = {}
-            for i, option in ipairs(baseBuildOptions) do
-                UnitDefs[unit].buildoptions[i] = option
-            end
-            table.insert(UnitDefs[unit].buildoptions, faction.."ap")
-            table.insert(UnitDefs[unit].buildoptions, faction.."vp")
-        end
-    end
+	local short = { faction .. 'apt3', faction .. 'afust3', faction .. 'gatet3', faction .. 'nanotct2', faction .. 'wint2', faction .. 'ap', faction .. 'vp' }
+	local additionalBuildOptions = {}
+	if faction == 'arm' then
+		additionalBuildOptions = { 'armminivulc', 'armbotrail', 'armannit3', 'armmmkrt3', 'armshockwave', 'armlwall' }
+	elseif faction == 'cor' then
+		additionalBuildOptions = { 'corminibuzz', 'corhllllt', 'cordoomt3', 'cormmkrt3', 'cormwall' }
+	elseif faction == 'leg' then
+		additionalBuildOptions = { 'legministarfall', 'legadveconvt3', 'legmohocon', 'legrwall' }
+	end
+
+	-- Combine the two tables
+	local addBuildoptions = {}
+	for _, option in ipairs(short) do
+		table.insert(addBuildoptions, option)
+	end
+	for _, option in ipairs(additionalBuildOptions) do
+		table.insert(addBuildoptions, option)
+	end
+
+	if UnitDefs[faction .. 'nanotct2'] then
+		UnitDefs[faction .. 'nanotct2'].buildoptions = deepcopy(UnitDefs[faction .. 'ack'].buildoptions)
+		for _, option in ipairs(addBuildoptions) do
+			table.insert(UnitDefs[faction .. 'nanotct2'].buildoptions, option)
+		end
+	end
 end
