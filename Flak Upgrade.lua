9a5b719e5aa87e7b117467c33bfd9906db797cef
Pluto <80173508+Makix1357@users.noreply.github.com>
2025-05-16 11:05:32 +0200

    Create Flak Upgrade.lua
    init

---
+++ Flak Upgrade.lua
@@ -0,0 +1,24 @@
+-- FlakUpgrade
+{
+armflak = {
+    weapondefs = {
+        armflak_gun  = {
+            areaofeffect = 500,
+        },
+    },
+},
+corflak = {
+    weapondefs = {
+        armflak_gun  = {
+            areaofeffect = 500,
+        },
+    },
+},
+legflak = {
+    weapondefs = {
+        legflak_gun  = {
+            areaofeffect = 350,
+        },
+    },
+}
+}
